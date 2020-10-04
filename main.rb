374547be69885489eb4657630ee68c1f762c102b
d-saori <60836449+d-saori@users.noreply.github.com>
2020-10-04 12:17:42 +0900

    key修正

--- main.rb
+++ main.rb
@@ -15,8 +15,8 @@ end
 
 def configure
   Trello.configure do |config|
-    config.developer_public_key = '58737dfe4a44de11b86b0d46738da1d2'
-    config.member_token = 'd0ce9cef56605cc2ed6f4ae6cec8e6067a055a6113768a7b9c33d0e49c553c0f'
+    config.developer_public_key = ENV['TRELLO_PUBLIC_KEY']
+    config.member_token = ENV['TRELLO_MEMBER_TOKEN']
   end
 end
 
