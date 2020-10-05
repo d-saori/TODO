d9cf79c0fa4a51f6cd921338369cd1979f048303
d-saori <60836449+d-saori@users.noreply.github.com>
2020-10-05 23:40:53 +0900

    classを使ったコードへ修正

--- main.rb
+++ main.rb
@@ -1,32 +1,69 @@
 require 'trello'
 require 'pry-byebug'
 
-def puts_todos
-  configure
-  cards = get_cards
+# def puts_todos
+#   configure
+#   cards = get_cards
 
-  puts '---------------------------'
-  puts '【TODO】'
-  cards.each do |card|
-    puts card.name
+#   puts '---------------------------'
+#   puts '【TODO】'
+#   cards.each do |card|
+#     puts card.name
+#   end
+#   puts '---------------------------'
+# end
+
+# def configure
+#   Trello.configure do |config|
+#     config.developer_public_key = ENV['TRELLO_PUBLIC_KEY']
+#     config.member_token = ENV['TRELLO_MEMBER_TOKEN']
+#   end
+# end
+
+# def get_cards
+#   member = Trello::Member.find('account')
+#   boards = member.boards
+#   board = boards.find { |board| board.name == 'TODO' }
+#   list = board.lists[0]
+#   cards = list.cards
+#   cards
+# end
+
+# puts_todos
+
+class TodaManager
+  attr_reader :account, :board_name
+
+  def initialize()
+    @account = account
+    @board_name = board_name
+    Trello.configure do |config|
+      config.developer_public_key = ENV['TRELLO_PUBLIC_KEY']
+      config.member_token = ENV['TRELLO_MEMBER_TOKEN']
+    end
   end
-  puts '---------------------------'
-end
 
-def configure
-  Trello.configure do |config|
-    config.developer_public_key = ENV['TRELLO_PUBLIC_KEY']
-    config.member_token = ENV['TRELLO_MEMBER_TOKEN']
+  def puts_todos
+    cards = get_cards
+    
+    puts '---------------------------'
+    puts '【TODO】'
+    cards.each do |card|
+      puts card.name
+    end
+      puts '---------------------------'
+  end
+    
+  private
+    
+  def get_cards
+    member = Trello::Member.find('account')
+    boards = member.boards
+    board = boards.find { |board| board.name == board_name }
+    list = board.lists[0]
+    cards = list.cards
   end
 end
-
-def get_cards
-  member = Trello::Member.find('sdx53779616')
-  boards = member.boards
-  board = boards.find { |board| board.name == 'TODO' }
-  list = board.lists[0]
-  cards = list.cards
-  cards
-end
-
+  
+tm = TodaManager.new('acount', 'TODO アプリ')
 puts_todos
